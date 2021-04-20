f6dabf1313796c8b2f5f40ce451c0777403ba428
YunSunWoong <yunsw0330@naver.com>
2021-04-20 22:20:10 +0900

    q

---
+++ js/const.js
@@ -0,0 +1,11 @@
+const number = 10;
+console.log(number);
+
+const number = 20;
+console.log(number);
+
+// 변수 중복선언 불가
+// 변수의 할당된 데이터가 변경되지 않는다.
+
+// var, let 키워드는 변수선언
+// const 키워드는 상수선언
---
+++ js/let.js
@@ -0,0 +1,7 @@
+let num = 10;
+console.log(num);
+
+let num = 20;
+console.log(num);
+
+// 변수 중복선언 불가
